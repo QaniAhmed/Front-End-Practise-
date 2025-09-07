a2b1bd48142c086cc4dc56d1b5abca3122ffd0db
ABDULQANI AHMED <153436304+QaniAhmed@users.noreply.github.com>
2025-09-07 22:14:53 +0300

    Add files via upload

---
+++ Css Theme Switcher Project/js.js
@@ -0,0 +1,6 @@
+function ChnageBackGround() {
+  document.querySelector("body").classList.toggle("invisible");
+  document.querySelector(".fa-sun").classList.toggle("sun");
+  document.querySelector(".fa-moon").classList.toggle("moon");
+  document.querySelector("div").classList.toggle("changeBar");
+}
