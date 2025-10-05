d0ea579baa88728af26305abf98cd097ad1800e2
ABDULQANI AHMED <153436304+QaniAhmed@users.noreply.github.com>
2025-10-05 19:58:51 +0300

    CSS gradients
    A collection of beautiful animated CSS gradients that can be easily copied to the clipboard with a single click.

---
+++ GradientPalette/script.js
@@ -0,0 +1,341 @@
+let stylesList = {
+  gradient1: [
+    `background: linear-gradient(300deg, #cdcccc, #8695f8, #cdcccc);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient2: [
+    `background: linear-gradient(268deg,rgba(247, 186, 186, 1) 56%, rgba(253, 45, 45, 1) 100%);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient3: [
+    `background: linear-gradient(268deg,rgba(186, 207, 247, 1) 50%, rgba(253, 45, 45, 1) 100%);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient4: [
+    `background: linear-gradient(90deg,rgba(1, 36, 0, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient5: [
+    `background: linear-gradient(90deg,rgba(6, 39, 54, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(237, 221, 83, 1) 100%);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient6: [
+    `background: linear-gradient(90deg,rgba(23, 25, 26, 1) 0%, rgba(94, 87, 199, 1) 50%, rgba(237, 221, 83, 1) 100%);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient7: [
+    `background: linear-gradient(90deg,rgba(23, 25, 26, 1) 0%, rgba(152, 87, 199, 1) 50%, rgba(237, 83, 111, 1) 100%);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient8: [
+    `background: linear-gradient(120deg, #00c6ff, #0072ff);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient9: [
+    `background: linear-gradient(135deg, #f7971e, #ffd200);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient10: [
+    `background: linear-gradient(145deg, #00b09b, #96c93d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient11: [
+    `background: linear-gradient(160deg, #a1c4fd, #c2e9fb);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient12: [
+    `background: linear-gradient(120deg, #ffecd2, #fcb69f);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient13: [
+    `background: linear-gradient(120deg, #ff9a9e, #fad0c4);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient14: [
+    `background: linear-gradient(120deg, #a18cd1, #fbc2eb);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient15: [
+    `background: linear-gradient(120deg, #f6d365, #fda085);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient16: [
+    `background: linear-gradient(120deg, #84fab0, #8fd3f4);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient17: [
+    `background: linear-gradient(120deg, #cfd9df, #e2ebf0);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient18: [
+    `background: linear-gradient(120deg, #ff6a00, #ee0979);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient19: [
+    `background: linear-gradient(120deg, #43e97b, #38f9d7);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient20: [
+    `background: linear-gradient(120deg, #fa709a, #fee140);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient21: [
+    `background: linear-gradient(120deg, #30cfd0, #330867);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient22: [
+    `background: linear-gradient(120deg, #5ee7df, #b490ca);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient23: [
+    `background: linear-gradient(120deg, #667eea, #764ba2);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient24: [
+    `background: linear-gradient(120deg, #2af598, #009efd);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient25: [
+    `background: linear-gradient(120deg, #ff0844, #ffb199);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient26: [
+    `background: linear-gradient(120deg, #ff512f, #dd2476);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient27: [
+    `background: linear-gradient(120deg, #24c6dc, #514a9d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient28: [
+    `background: linear-gradient(120deg, #ee9ca7, #ffdde1);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient29: [
+    `background: linear-gradient(120deg, #2193b0, #6dd5ed);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient30: [
+    `background: linear-gradient(120deg, #cc2b5e, #753a88);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient31: [
+    `background: linear-gradient(120deg, #42275a, #734b6d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient32: [
+    `background: linear-gradient(120deg, #de6262, #ffb88c);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient33: [
+    `background: linear-gradient(120deg, #06beb6, #48b1bf);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient34: [
+    `background: linear-gradient(120deg, #56ccf2, #2f80ed);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient35: [
+    `background: linear-gradient(120deg, #a8ff78, #78ffd6);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient36: [
+    `background: linear-gradient(120deg, #fdfbfb, #ebedee);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient37: [
+    `background: linear-gradient(120deg, #ff7e5f, #feb47b);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient38: [
+    `background: linear-gradient(120deg, #ff9a9e, #fecfef);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient39: [
+    `background: linear-gradient(120deg, #89f7fe, #66a6ff);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient40: [
+    `background: linear-gradient(120deg, #fbd3e9, #bb377d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient41: [
+    `background: linear-gradient(120deg, #141e30, #243b55);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient42: [
+    `background: linear-gradient(120deg, #e0c3fc, #8ec5fc);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient43: [
+    `background: linear-gradient(120deg, #ff758c, #ff7eb3);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient44: [
+    `background: linear-gradient(120deg, #f0c27b, #4b1248);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient45: [
+    `background: linear-gradient(120deg, #e65c00, #f9d423);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient46: [
+    `background: linear-gradient(120deg, #c33764, #1d2671);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient47: [
+    `background: linear-gradient(120deg, #ed213a, #93291e);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient48: [
+    `background: linear-gradient(120deg, #d1913c, #ffd194);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient49: [
+    `background: linear-gradient(120deg, #1f4037, #99f2c8);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient50: [
+    `background: linear-gradient(120deg, #3a1c71, #d76d77, #ffaf7b);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient51: [
+    `background: linear-gradient(120deg, #ff758c, #ff7eb3, #fad0c4);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient52: [
+    `background: linear-gradient(270deg, #8e2de2, #4a00e0);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient53: [
+    `background: linear-gradient(45deg, #00c6ff, #0072ff);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient54: [
+    `background: linear-gradient(90deg, #f7971e, #ffd200);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient55: [
+    `background: linear-gradient(135deg, #f953c6, #b91d73);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient56: [
+    `background: linear-gradient(135deg, #00b09b, #96c93d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient57: [
+    `background: linear-gradient(135deg, #f12711, #f5af19);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient58: [
+    `background: linear-gradient(225deg, #654ea3, #eaafc8);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient59: [
+    `background: linear-gradient(225deg, #ff9966, #ff5e62);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient60: [
+    `background: linear-gradient(225deg, #00f260, #0575e6);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient61: [
+    `background: linear-gradient(225deg, #2193b0, #6dd5ed);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient62: [
+    `background: linear-gradient(45deg, #cc2b5e, #753a88);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient63: [
+    `background: linear-gradient(90deg, #43cea2, #185a9d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient64: [
+    `background: linear-gradient(90deg, #f79d00, #64f38c);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient65: [
+    `background: linear-gradient(120deg, #c31432, #240b36);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient66: [
+    `background: linear-gradient(120deg, #42275a, #734b6d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient67: [
+    `background: linear-gradient(120deg, #4776e6, #8e54e9);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient68: [
+    `background: linear-gradient(120deg, #f7971e, #ffd200);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient69: [
+    `background: linear-gradient(120deg, #1d4350, #a43931);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient70: [
+    `background: linear-gradient(120deg, #ee9ca7, #ffdde1);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient71: [
+    `background: linear-gradient(120deg, #4e54c8, #8f94fb);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient72: [
+    `background: linear-gradient(120deg, #43cea2, #185a9d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient73: [
+    `background: linear-gradient(120deg, #ff416c, #ff4b2b);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient74: [
+    `background: linear-gradient(120deg, #1f4037, #99f2c8);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient75: [
+    `background: linear-gradient(120deg, #4568dc, #b06ab3);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient76: [
+    `background: linear-gradient(120deg, #0f2027, #203a43, #2c5364);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient77: [
+    `background: linear-gradient(120deg, #373b44, #4286f4);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient78: [
+    `background: linear-gradient(120deg, #ee0979, #ff6a00);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient79: [
+    `background: linear-gradient(120deg, #ff512f, #dd2476);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient80: [
+    `background: linear-gradient(120deg, #1a2a6c, #b21f1f, #fdbb2d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient81: [
+    `background: linear-gradient(120deg, #fdfc47, #24fe41);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient82: [
+    `background: linear-gradient(120deg, #3a1c71, #d76d77, #ffaf7b);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient83: [
+    `background: linear-gradient(120deg, #0cebeb, #20e3b2, #29ffc6);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient84: [
+    `background: linear-gradient(120deg, #5f2c82, #49a09d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient85: [
+    `background: linear-gradient(120deg, #00b4db, #0083b0);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient86: [
+    `background: linear-gradient(120deg, #f46b45, #eea849);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient87: [
+    `background: linear-gradient(120deg, #e1eec3, #f05053);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient88: [
+    `background: linear-gradient(120deg, #42275a, #734b6d);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient89: [
+    `background: linear-gradient(120deg, #2c3e50, #4ca1af);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient90: [
+    `background: linear-gradient(120deg, #de6161, #2657eb);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient91: [
+    `background: linear-gradient(120deg, #02aab0, #00cdac);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient92: [
+    `background: linear-gradient(120deg, #396afc, #2948ff);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient93: [
+    `background: linear-gradient(120deg, #00c3ff, #ffff1c);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient94: [
+    `background: linear-gradient(120deg, #fffc00, #ffffff);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient95: [
+    `background: linear-gradient(120deg, #a8ff78, #78ffd6);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient96: [
+    `background: linear-gradient(120deg, #f00000, #ffed00);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient97: [
+    `background: linear-gradient(120deg, #833ab4, #fd1d1d, #fcb045);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient98: [
+    `background: linear-gradient(120deg, #12c2e9, #c471ed, #f64f59);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient99: [
+    `background: linear-gradient(120deg, #000046, #1cb5e0);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+  gradient100: [
+    `background: linear-gradient(120deg, #fc5c7d, #6a82fb);animation: gradient-animation 18s ease infinite;background-size: 180% 180%;`,
+  ],
+};
+let animation = `@keyframes gradient-animation {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}"`;
+
+function CopyDesign(e) {
+  let class_name = e.className;
+  console.log(typeof e.className);
+  console.log(e.className);
+  let fullDesign = stylesList[class_name] + "\n" + animation;
+  console.log(fullDesign);
+  copyMsg(fullDesign);
+}
+function copyMsg(textToCopy) {
+  // Copy to clipboard
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      // Show message
+      let copyMsg = document.querySelector(".copyContainer");
+      copyMsg.classList.remove("hide");
+      copyMsg.innerHTML = "Copied to clipboard!";
+
+      // Hide message after 1 second
+      setTimeout(() => {
+        copyMsg.classList.add("hide");
+      }, 1000);
+    })
+    .catch((err) => {
+      console.error("Clipboard copy failed:", err);
+    });
+}
